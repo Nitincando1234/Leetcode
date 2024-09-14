025f1b278c0ed457b165fe8df2923ad652939c07
Nitin Sharma <144478626+Nitincando1234@users.noreply.github.com>
2024-09-14 22:15:22 +0530

    Time: 61 ms (8.82%), Space: 17 MB (83.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,27 @@
 class Solution:
+    def alnum_condition(self, s: str):
+        if (("A" <= s <= "Z") or ("a" <= s <= "z") or ("0" <= s <= "9")): return True
+        return False
+    
+    def lower(self, s: str):
+        if "A" <= s <= "Z":
+            return chr(ord(s) + 32)       # ord return the ASCII value of str
+        return s
+    
     def isPalindrome(self, s: str) -> bool:
-        newStr = ""
-        for i in range(len(s)):
-            if s[i].isalnum(): 
-                newStr += s[i].lower()
-        return newStr == newStr[:: -1]
+        left, right = 0, len(s) - 1
+        test = 0
+        while left <= right:
+            if not self.alnum_condition(s[left]):
+                left += 1
+                continue
+            elif not self.alnum_condition(s[right]):
+                right -= 1
+                continue
+            
+            if self.lower(s[left]) != self.lower(s[right]): return False
+            
+            left += 1
+            right -= 1
+        
+        return True
