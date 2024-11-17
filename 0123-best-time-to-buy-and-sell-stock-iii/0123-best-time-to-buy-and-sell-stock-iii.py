3febb66a2d23b1b6983c19f12000600676c09428
Nitin Sharma <144478626+Nitincando1234@users.noreply.github.com>
2024-11-17 22:30:30 +0530

    Time: 209 ms (82.17%), Space: 28.1 MB (85.91%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices: return 0
+        A = -prices[0]
+        B, C, D = [float("-inf")] * 3
+        
+        for price in prices:
+            A = max(A, -price)
+            B = max(B, A + price)
+            C = max(C, B - price)
+            D = max(D, C + price)
+        
+        return D
